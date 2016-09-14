b88a145a2a8894d5caeed580a2c1139d6064c297
yu lan <lanyujump@gmail.com>
2016-09-14 22:48:29 +0000

    prepare done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rottenpotatoes::Application.config.secret_token = 'bba1ecadcefb512fd62d9e7a01cf44e0a9a4f525a648477d61a8b55ba538d08da92fd1b930ce7c0793fde6e0cfc3f72c1fef8df83591a46066f877ac954f35b7'
---
+++ db/migrate/20160914215956_add_fields_to_movies.rb
@@ -0,0 +1,8 @@
+class AddFieldsToMovies < ActiveRecord::Migration
+  def change
+    add_column :movies, :title, :string
+    add_column :movies, :release_date, :datetime
+    add_column :movies, :rating, :string
+    add_column :movies, :description, :text
+  end
+end
